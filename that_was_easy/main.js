34c7e1c9f17dab06fadc106562dda47946c6a3bd
Tanay Sahasrabudhe <tanays118@gmail.com>
2017-06-29 18:10:22 +0000

    Dawg It Up

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,2 +1,15 @@
-var thatWasEasy = new Audio("that_was_easy(1).mp3");
-thatWasEasy.play();
+function sayThatWasEasy() {
+    var thatWasEasy = new Audio("that_was_easy(1).mp3");
+    thatWasEasy.play();
+}
+
+$("#easy").on("click", sayThatWasEasy);
+$(document).keypress(delegateKeypress);
+
+function.delegateKeypress(event) {
+    if (event.charCode == 32) {
+       $("#easy").trigger("click");   
+    }
+}
+
+
